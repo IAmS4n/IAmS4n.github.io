7a413223c5f2f79b02920acb740da2141736a1da
ehsan MAE <ehsan.m.a.e@gmail.com>
2020-07-23 22:27:59 +0430

    [Animation speed]]

--- assets/js/app.js
+++ assets/js/app.js
@@ -70,7 +70,7 @@ particlesJS('particles-js',
       },
       move: {
         enable: true,
-        speed: 5,
+        speed: 3,
         direction: 'none',
         random: false,
         straight: false,
